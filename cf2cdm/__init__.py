10d3b6c6a1f298ae8262500d1851c1548f874fb3
Alessandro Amici <a.amici@bopen.eu>
2023-10-15 20:12:25 +0200

    Fix accessing __version__

--- cf2cdm/__init__.py
+++ cf2cdm/__init__.py
@@ -22,9 +22,7 @@ except ImportError:  # pragma: no cover
     # Local copy or not installed with setuptools
     __version__ = "999"
 
-__all__ = ["__version__"]
-
 from .cfcoords import translate_coords
 from .datamodels import CDS, ECMWF
 
-__all__ = ["CDS", "ECMWF", "translate_coords"]
+__all__ = ["CDS", "ECMWF", "__version__", "translate_coords"]
